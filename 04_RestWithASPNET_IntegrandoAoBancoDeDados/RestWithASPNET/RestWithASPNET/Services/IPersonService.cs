459267c681b27068346d48dc7492d42ab71eeea1
Vandinete <vandysouza2@gmail.com>
2023-07-17 22:37:48 -0300

    Integrando a aplicação ao banco de dados com os quatro verbos

---
+++ 04_RestWithASPNET_IntegrandoAoBancoDeDados/RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Service
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
