e66a6ad959afdf5b3305ef327a1845bf8724edeb
Vandinete <vandysouza2@gmail.com>
2023-07-17 23:45:07 -0300

    Versionamento de endpoint

---
+++ 05_RestWithASPNET_VersionandoAPI/RestWithASPNET/RestWithASPNET/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Service;
+using RestWithASPNET.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container...
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.Parse("7.0")));
+
+//Versioning API
+builder.Services.AddApiVersioning();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
