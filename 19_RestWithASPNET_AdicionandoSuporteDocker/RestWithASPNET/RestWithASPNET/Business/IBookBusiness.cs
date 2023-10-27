a0bcf9ba29ac9f063f999cca77dad4dc0c71f70f
Vandinete <vandysouza2@gmail.com>
2023-10-27 00:11:03 -0300

    Adding Docker Support

---
+++ 19_RestWithASPNET_AdicionandoSuporteDocker/RestWithASPNET/RestWithASPNET/Business/IBookBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNET.Data.VO;
+
+namespace RestWithASPNET.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindAll();
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
---
+++ 19_RestWithASPNET_AdicionandoSuporteDocker/RestWithASPNET/RestWithASPNET/Data/VO/UserVO.cs
@@ -0,0 +1,8 @@
+﻿namespace RestWithASPNET.Data.VO
+{
+    public class UserVO
+    {
+        public string Password { get; set; }
+        public string UserName { get;  set; }
+    }
+}
