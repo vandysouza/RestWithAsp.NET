39a5c06c756514c4a113028d3b53117c4c84c440
Vandinete Souza <vandysouza21@outlook.com>
2024-01-07 18:59:08 -0300

    Ajuste no projeto para conexao com banco de dados SQL Server

---
+++ BONUS_RestWithASPNET_SQL_Server/RestWithASPNET/RestWithASPNET/Model/Context/MSSQLContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNET.Model.Context
+{
+    public class MSSQLContext : DbContext
+    {
+        public MSSQLContext()
+        {
+
+        }
+
+        public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ BONUS_RestWithASPNET_SQL_Server/RestWithASPNET/RestWithASPNET/Repository/IPersonRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Repository
+{
+    public interface IPersonRepository : IRepository<Person>
+    {
+        Person Disable(long id);
+        List<Person> FindByName(string firstName, string secondName);
+    }
+}
